6bb3c51b24f3bbe82f4e95ac64819f46ca80c5d6
Adityawan Chandra <adityawancha@gmail.com>
2020-07-10 22:30:19 +0700

    Restore x to X (#4)
    Co-authored-by: hamdanidev <hamdani@aegis.co.id>

--- src/Components/TodoApp/ItemList.js
+++ src/Components/TodoApp/ItemList.js
@@ -55,7 +55,7 @@ class Item extends Component {
           onClick={e => this.props.onClick(e.target.value)}
           value={this.props.keys}
         >
-          x
+          X
         </button>
         <input
           type="checkbox"
